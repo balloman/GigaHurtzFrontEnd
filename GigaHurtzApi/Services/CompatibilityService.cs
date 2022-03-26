69891b056810f28d4cd9f936604f6dc3b301aa4e
Bernard Allotey <benard.allotey4@gmail.com>
2022-03-26 17:50:51 -0500

    Started compat api

---
+++ GigaHurtzApi/Services/CompatibilityService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Immutable;
+using GigaHurtz.Common.Models;
+
+namespace GigaHurtzApi.Services;
+
+public class CompatibilityService
+{
+    private readonly IDbService _dbService;
+    
+    public CompatibilityService(IDbService dbService)
+    {
+        _dbService = dbService;
+    }
+    
+    /// <summary>
+    /// Gets the compatibility for a refugee
+    /// </summary>
+    /// <param name="uid"></param>
+    /// <returns>Immutable list of compat objects</returns>
+    public IImmutableList<Compatibility> GetCompatForUser(string uid)
+    {
+        return null;
+    }
+}
